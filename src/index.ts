100aa82af19bdefb212832efd9898ee34b43f1a1
suissAI <jnascimento@gmail.com>
2026-09-14 05:28:33 -0300

    feat: export semantic validation API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types.js";
 export * from "./canonical.js";
 export * from "./validator.js";
+export * from "./semantic-validator.js";
 export * from "./completeness.js";
 export * from "./materialize.js";
 export * from "./conformance.js";
