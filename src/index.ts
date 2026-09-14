2c1cf56f75f4c84faab5482a2b2a1eb35527f929
suissAI <jnascimento@gmail.com>
2026-09-14 05:30:02 -0300

    feat: export enhanced Markdown adapter

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,6 @@ export * from "./conformance.js";
 export * from "./interview.js";
 export * from "./migrate.js";
 export * from "./custom-requirements.js";
+export * from "./markdown.js";
 export * from "./allasdsl/parser.js";
 export * from "./adapters/index.js";
